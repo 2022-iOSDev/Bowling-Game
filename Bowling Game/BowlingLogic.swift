b764018c998a57c75cf665e227e4a31354b148e2
2022-iOSDev <vigneshkumarhs@gmail.com>
2022-07-04 23:30:58 +0530

    Gutter game logic implemented

--- Bowling Game/BowlingLogic.swift
+++ Bowling Game/BowlingLogic.swift
@@ -9,4 +9,16 @@ import Foundation
 
 class BowlingLogic {
     
+    var finalScore = 0
+    
+    // Call this for every fresh roll.
+    func roll(_ pinCount: Int) {
+        finalScore = finalScore + pinCount
+    }
+    
+    
+    // Call this to calculate the total score after completing all the rolls.
+    func calculateScore() -> Int{
+        return finalScore
+    }
 }
