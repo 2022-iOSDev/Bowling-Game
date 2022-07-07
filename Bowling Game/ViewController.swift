e23aa21f708e49da61057a12f30cba0550c002b5
2022-iOSDev <vigneshkumarhs@gmail.com>
2022-07-07 21:36:45 +0530

    Creating view model instance and overriding touchesBegan

--- Bowling Game/ViewController.swift
+++ Bowling Game/ViewController.swift
@@ -9,25 +9,38 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // Outlets
     
     @IBOutlet weak var rollTextfield: UITextField!
     @IBOutlet weak var rollButton: UIButton!
     @IBOutlet weak var calculateScoreButton: UIButton!
     
+    // ViewModel
+    var viewModel: BowlingLogic?
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        viewModel = BowlingLogic()
 
         rollButton.createBorderShadow()
         calculateScoreButton.createBorderShadow()
     }
 
+    
+    // Button Actions
+    
     @IBAction func rollButtonPressed(_ sender: Any) {
     }
     
     @IBAction func calculateScorePressed(_ sender: Any) {
     }
     
-    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        rollTextfield.resignFirstResponder()
+        self.view.endEditing(true)
+    }
 }
 
